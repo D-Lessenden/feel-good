65e9cdb7a9c2e77f9c1a386197379c2c407e220f
Daniel Lessenden <dless27@gmail.com>
2021-02-04 17:24:56 -0700

    Login and sign up pages

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
 
+    def login(self, username, password):
+        with open("users.json") as file:
+            users = json.load(file)
+        if username in users and users[username]['password'] == password:
+            self.manager.current = 'login_screen_success'
+        else:
+            self.ids.login_wrong = "Wrong username or password"
+
+
 class RootWidget(ScreenManager):
     pass
 
@@ -23,10 +32,19 @@ class SignUpScreen(Screen):
 
         with open("users.json", 'w') as file:
             json.dump(users, file)
+        
         self.manager.current = "sign_up_screen_success"
 
 class SignUpScreenSuccess(Screen):
-    pass
+    def go_to_login(self):
+
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
 
 class MainApp(App):
     def build(self):
